e9f27f2b6d3b71bac537b657861a428389863d54
shishir198918 <69082210+shishir198918@users.noreply.github.com>
2024-10-30 08:10:27 +0530

    number parser

--- parser.py
+++ parser.py
@@ -1,78 +1,104 @@
 def parse_null(input_string):
     """Parses null
-    
+
     >>> parse_null("null rest")
     (None, " rest")
 
     >>> parse_null("something else")
     None
-    
+
     """
-    pass
+    if input_string[:4] == "null":
+        return None, input_string[4:]
+
 
 def parse_bool(input_string):
     """Parses boolean
-    
+
     >>> parse_bool("true rest")
     (True, " rest")
 
     >>> parse_bool("something else")
     None
-    
+
     """
-    pass
+    if input_string[:4] == "true":
+        return True, input_string[4:]
+    elif input_string[:5] == "false":
+        return False, input_string[:5]
+
 
 def parse_number(input_string):
     """Parses number
-    
+
     >>> parse_number("123.456 rest")
     (123.456, " rest")
 
     >>> parse_number("something else")
     None
-    
+
     """
-    pass
+    digit = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "e", "E", "-", "+", "."}
+    if input_string[:2] == "00" or input_string[1:3] == "00" or input_string[0] == " ":
+        return input_string
+    index = 1
+    if input_string[0] != "+" and input_string[0] in digit:
+        while index < len(input_string):
+            if input_string[index] in digit:
+                index = index + 1
+            else:
+                break
+
+        try:
+            return int(input_string[:index]), input_string[index:]
+        except ValueError:
+            return float(input_string[:index]), input_string[index:]
+    else:
+        input_string
+
 
 def parse_string(input_string):
     """Parses string
-    
+
     >>> parse_string('"abc" rest')
     ("abc", " rest")
 
     >>> parse_string("something else")
     None
-    
+
     """
     pass
 
+
 def parse_array(input_string):
     """Parses array
-    
+
     >>> parse_array("[1, 2, 3] rest")
     ([1, 2, 3], " rest")
 
     >>> parse_array("something else")
     None
-    
+
     """
     pass
 
+
 def parse_object(input_string):
     """Parses object
-    
+
     >>> parse_object('{"key": "value", "name": 1337} rest')
     ({"key": "value", "name": 1337}, " rest")
 
     >>> parse_object("something else")
     None
-    
+
     """
     pass
 
+
 def parse_json(input_string):
     """Parses json
-    
+
     >>> parse_json('[{"my": "value"}, 2, "just string"]')
     [{"my": "value"}, 2, "just string"]
 
@@ -83,6 +109,8 @@ def parse_json(input_string):
     """
     pass
 
+
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
